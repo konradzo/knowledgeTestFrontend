cb458c1b0d8e3c8f8546d57a50c38916e4bfbe97
Konrad Żochowski <konrad.zochowski@newspoint.pl>
2020-08-04 19:39:51 +0200

    category list component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CategoryListComponent } from './components/category-list/category-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoryListComponent
   ],
   imports: [
     BrowserModule
